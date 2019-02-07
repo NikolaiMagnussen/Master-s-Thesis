152b85bddd2443a8fb91cb65beced862f4ffaa8b
Nikolai Magnussen <nikolai@magnussen.tf>
2019-02-07 14:05:57 +0100

    Implement load balancing for proxy server

--- proxy/src/unikernel.ml
+++ proxy/src/unikernel.ml
@@ -10,7 +10,18 @@ module Proxy (CON : Conduit_mirage.S) = struct
     let ip = Ipaddr.of_string_exn ip_str in
     Hashtbl.add table host (fun ~(port:int) -> (`TCP (ip, port) : Conduit.endp))
 
-  let route_table = let tbl = Hashtbl.create 3 in
+  let round_robin =
+    let tbl = Hashtbl.create 6 in
+    Hashtbl.add tbl `None (0, 0, []);
+    Hashtbl.add tbl `Unclassified (0, 0, []);
+    Hashtbl.add tbl `Restricted (0, 0, []);
+    Hashtbl.add tbl `Confidential (0, 0, []);
+    Hashtbl.add tbl `Secret (0, 0, []);
+    Hashtbl.add tbl `TopSecret (0, 0, []);
+    tbl
+
+  let route_table =
+    let tbl = Hashtbl.create 3 in
     add_entry "proxy.local" "10.0.0.2" tbl;
     add_entry "auth.local" "10.0.0.3" tbl;
     add_entry "static.local" "10.0.0.4" tbl;
@@ -26,19 +37,35 @@ module Proxy (CON : Conduit_mirage.S) = struct
     let uri = build_uri "auth.local" "/" in
     Client.get ~ctx uri ~headers >>= fun (resp, _body) ->
     let headers = Response.headers resp in
-    let cap = match Header.get headers "capability" with
+    let cap = match Header.get headers "capabilities" with
       | Some cap -> Capability_j.capability_of_string cap
       | None -> `None 
     in Lwt.return cap
+
+  let hostname_of_cap cap num =
+    let cap_str = Capability_j.string_of_capability cap in
+    let cap = match String.split_on_char '"' cap_str with
+      | _ :: cap :: _ -> cap
+      | _ -> "None"
+    in
+    Printf.sprintf "%s%d.local" (String.lowercase_ascii cap) num
+
+  let add_cap_entry cap =
+    let (next, len, hosts) = Hashtbl.find round_robin cap in
+    let new_host = hostname_of_cap cap len in
+    Hashtbl.replace round_robin cap (next, len+1, new_host::hosts);
+    new_host
 
   let register_service headers =
     let ip = Header.get headers "ip-addr" in
     let cap = Header.get headers "capability" in
     match (ip, cap) with
     | (Some ip, Some cap) ->
-      add_entry cap ip route_table;
+      let capability = Capability_j.capability_of_string cap in
+      let new_host = add_cap_entry capability in
+      add_entry new_host ip route_table;
       S.respond ~status: `Accepted ~body: `Empty ()
-    | (_, _) -> S.respond ~status: `Unprocessable_entity ~body: `Empty ()
+    | (_, _) -> S.respond ~status: `Bad_request ~body: `Empty ()
 
   let get_routing_table () =
     let keys = Hashtbl.to_seq route_table in
@@ -51,18 +78,38 @@ module Proxy (CON : Conduit_mirage.S) = struct
     in
     Seq.fold_left map_keys_to_str "" keys
 
+  let get_round_robin_table () =
+    let kv = Hashtbl.to_seq round_robin in
+    let map_kv_to_str prev_str (k, (n, l, vs)) =
+      let v_str = List.fold_left (Printf.sprintf "%s, %s") "" vs in
+      Printf.sprintf "%s%s -> (%d, %d, %s)\n" prev_str (Capability_j.string_of_capability k) n l v_str
+    in
+    Seq.fold_left map_kv_to_str "" kv
+
+  let get_cap_host cap robin_table =
+    let (curr, len, hosts) = Hashtbl.find robin_table cap in
+    if len > 0 then
+      let next = (curr + 1) mod len in
+      Hashtbl.replace robin_table cap (next, len, hosts);
+      Some (List.nth hosts (curr mod len))
+    else
+      None
+
+  let forward_response (resp, body) =
+    let status = Response.status resp in
+    S.respond ~status ~body ()
+
   let handle path meth headers body conduit =
-    let uri = build_uri "static.local" path in
     let ctx = ctx conduit in
     get_capabilities headers ctx >>= fun cap ->
-    match (meth, path) with
-    | (`GET, "/register") -> register_service headers
-    | (`GET, "/routes") -> S.respond_string ~status: `OK ~body: (get_routing_table ()) ()
-    | (`GET, _) -> Client.get ~ctx uri
-    | (`POST, _) -> Client.post ~ctx ~body uri
-    | _ -> Client.get ~ctx uri
-      >>= fun (resp, body) ->
-      S.respond_string ~status: `OK ~body: "kake er godt" ()
+    let host = get_cap_host cap round_robin in
+    match (meth, path, host) with
+    | (`GET, "/register", _) -> register_service headers
+    | (`GET, "/routes", _) -> S.respond_string ~status: `OK ~body: (get_routing_table ()) ()
+    | (`GET, "/robin", _) -> S.respond_string ~status: `OK ~body: (get_round_robin_table ()) ()
+    | (`GET, _, Some host) -> Client.get ~ctx (build_uri host path) >>= forward_response
+    | (`POST, _, Some host) -> Client.post ~ctx ~body (build_uri host path) >>= forward_response
+    | _ -> S.respond ~status: `Not_found ~body: `Empty ()
 
   let start conduit =
     let callback _conn req body =
