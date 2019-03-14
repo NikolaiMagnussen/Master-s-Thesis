622525ee6126552b7575ea98b35583b8dfbea38b
Nikolai Magnussen <nikolai@magnussen.tf>
2019-03-14 15:16:44 +0100

    Add skeleton for vmmd

---
+++ vmmd/vmmd.ml
@@ -0,0 +1,17 @@
+open Bos
+open Rresult
+
+let new_tap br =
+  let rec free_tap n =
+    let tap_name = "tap" ^ string_of_int n in
+    match Bos.OS.Cmd.run Bos.Cmd.(v "ip" % "addr" % "show" % "dev" % tap_name) with
+    | Error _ -> tap_name
+    | Ok _ -> free_tap (succ n)
+  in
+  let tap = free_tap 0 in
+  Bos.OS.Cmd.run Bos.Cmd.(v "ip" % "tuntap" % "add" % tap % "mode" % "tap") >>= fun () ->
+  Bos.OS.Cmd.run Bos.Cmd.(v "ip" % "link" % "set" % tap % "master" % br) >>= fun () ->
+  Ok tap
+
+
+let () = ()
