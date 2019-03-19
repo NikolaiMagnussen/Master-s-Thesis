a70d5beb31ca37c1b2ab08530886958b61996ad6
Nikolai Magnussen <nikolai@magnussen.tf>
2019-03-19 20:40:40 +0100

    Add some more features to vmmd

--- vmmd/vmmd.ml
+++ vmmd/vmmd.ml
@@ -1,5 +1,7 @@
-open Bos
 open Rresult
+open Lwt
+open Cohttp
+open Cohttp_lwt_unix
 
 let new_tap br =
   let rec free_tap n =
@@ -9,15 +11,17 @@ let new_tap br =
     | Ok _ -> free_tap (succ n)
   in
   let tap = free_tap 0 in
-  Bos.OS.Cmd.run Bos.Cmd.(v "ip" % "tuntap" % "add" % tap % "mode" % "tap") >>= fun () ->
-  Bos.OS.Cmd.run Bos.Cmd.(v "ip" % "link" % "set" % tap % "master" % br) >>= fun () ->
-  Ok tap
+  match Bos.OS.Cmd.run Bos.Cmd.(v "ip" % "tuntap" % "add" % tap % "mode" % "tap") with
+  | Error a -> Error a
+  | Ok _ ->
+    match Bos.OS.Cmd.run Bos.Cmd.(v "ip" % "link" % "set" % tap % "master" % br) with
+    | Error a -> Error a
+    | Ok _ -> Ok tap
 
-(*
 let spawn kernel =
-  let tap = new_tap "br0" in
-  Bos.OS.Cmd.run Bos.Cmd.()
-*)
+  match new_tap "br0" with
+  | Error a -> Error a
+  | Ok tap -> Bos.OS.Cmd.run Bos.Cmd.(v "ls" % tap % kernel)
 
 (* Things we need to do:
  * - Create new tap
@@ -26,4 +30,16 @@ let spawn kernel =
  * - Destroy tap device
 *)
 
-let () = ()
+let server =
+  let callback _conn req body =
+    let uri = req |> Request.uri |> Uri.to_string in
+    let meth = req |> Request.meth |> Code.string_of_method in
+    let headers = req |> Request.headers |> Header.to_string in
+    body |> Cohttp_lwt.Body.to_string >|= (fun body ->
+        (Printf.sprintf "Uri: %s\nMethod: %s\nHeaders: %s\nBody: %s"
+           uri meth headers body))
+    >>= (fun body -> Server.respond_string ~status:`OK ~body ())
+  in
+  Server.create ~mode: (`TCP (`Port 8000)) (Server.make ~callback ())
+
+let () = ignore (Lwt_main.run server)
