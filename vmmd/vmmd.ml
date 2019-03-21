24f6d59e33dfd980a62338a18d7c53a899ec024a
Nikolai Magnussen <nikolai@magnussen.tf>
2019-03-21 21:41:14 +0100

    Add handlers for vmmd and implement some of them

--- vmmd/vmmd.ml
+++ vmmd/vmmd.ml
@@ -1,7 +1,6 @@
 open Rresult
-open Lwt
-open Cohttp
-open Cohttp_lwt_unix
+open Opium.Std
+open Lwt.Infix
 
 let new_tap br =
   let rec free_tap n =
@@ -23,6 +22,14 @@ let spawn kernel =
   | Error a -> Error a
   | Ok tap -> Bos.OS.Cmd.run Bos.Cmd.(v "ls" % tap % kernel)
 
+let spawn_level kernel level =
+  match new_tap "br0" with
+  | Error a -> Error a
+  | Ok tap -> Bos.OS.Cmd.run Bos.Cmd.(v "ls" % tap % kernel % level)
+
+let stop id =
+  Bos.OS.Cmd.run Bos.Cmd.(v "ls" % id)
+
 (* Things we need to do:
  * - Create new tap
  * - Spawn unikernel and attach the network tap
@@ -30,16 +37,121 @@ let spawn kernel =
  * - Destroy tap device
 *)
 
-let server =
-  let callback _conn req body =
-    let uri = req |> Request.uri |> Uri.to_string in
-    let meth = req |> Request.meth |> Code.string_of_method in
-    let headers = req |> Request.headers |> Header.to_string in
-    body |> Cohttp_lwt.Body.to_string >|= (fun body ->
-        (Printf.sprintf "Uri: %s\nMethod: %s\nHeaders: %s\nBody: %s"
-           uri meth headers body))
-    >>= (fun body -> Server.respond_string ~status:`OK ~body ())
-  in
-  Server.create ~mode: (`TCP (`Port 8000)) (Server.make ~callback ())
+type unikernel = {
+  name: string;
+  path: Fpath.t;
+  default: string;
+}
 
-let () = ignore (Lwt_main.run server)
+type running = {
+  name: string;
+  level: string;
+  id: string;
+}
+
+let unikernels = Hashtbl.create 1
+let running = Hashtbl.create 1
+
+let add_running name level =
+  if Hashtbl.mem unikernels name then
+    let id = Uuidm.(to_string (create `V4)) in
+    let kernel = {
+      name = name;
+      level = level;
+      id = id;
+    } in
+    Hashtbl.add running id kernel;
+    Some id
+  else
+    None
+
+let add_unikernel name path level =
+  let unikernel = {
+    name = name;
+    path = path;
+    default = level;
+  } in
+  Hashtbl.add unikernels name unikernel
+
+let del_unikernel name =
+  Hashtbl.remove unikernels name
+
+let edit_unikernel name path level =
+  let unikernel = {
+    name = name;
+    path = path;
+    default = level;
+  } in
+  Hashtbl.replace unikernels name unikernel
+
+let extract_body req =
+  App.json_of_body_exn req |> Lwt.map (fun json ->
+      let json = Ezjsonm.value json in
+      let name = Ezjsonm.(get_string (find json ["name"])) in
+      let path = Ezjsonm.(get_string (find json ["path"])) in
+      let level = Ezjsonm.(get_string (find json ["level"])) in
+      (name, path, level))
+
+let register_unikernel = post "/" begin fun req ->
+    extract_body req >>=
+    fun (_name, _path, _level) ->
+    `String "kake er godt" |> respond'
+  end
+
+let update_unikernel = put "/:name" begin fun req ->
+    extract_body req >>=
+    fun (name, _path, _level) ->
+    `String ("update unikernel " ^ name) |> respond'
+  end
+
+let delete_unikernel = delete "/:name" begin fun req ->
+    `String ("delete unikernel " ^ param req "name") |> respond'
+  end
+
+let list_unikernels = get "/" begin fun _req ->
+    let transform = fun (_name, {name; path; default}) ->
+      "name: " ^ name ^ ", path: " ^ (Fpath.to_string path) ^ ", default: " ^ default
+    in
+    let str_seq = Seq.map transform (Hashtbl.to_seq unikernels) in
+    let unikernel_str = Seq.fold_left (^) "" str_seq in
+    `String unikernel_str |> respond'
+  end
+
+let start_unikernel = get "/start/:name" begin fun req ->
+    let name = param req "name" in
+    let code = match spawn name with
+      | Error _ -> `Not_found
+      | Ok _ -> `OK
+    in
+    `String ("start unikernel " ^ name ^ " with default level") |> respond' ~code
+  end
+
+let start_unikernel_level = get "/start/:name/:level" begin fun req ->
+    let name = param req "name" in
+    let level = param req "level" in
+    let code = match spawn_level name level with
+      | Error _ -> `Not_found
+      | Ok _ -> `OK
+    in
+    `String ("start unikernel " ^ name ^ " with level " ^ level) |> respond' ~code
+  end
+
+let stop_unikernel = get "/stop/:id" begin fun req ->
+    let id = param req "id" in
+    let code = match stop id with
+      | Error _ -> `Not_found
+      | Ok _ -> `OK
+    in
+    `String ("stop unikernel " ^ id) |> respond' ~code
+  end
+
+let () =
+  App.empty
+  |> register_unikernel
+  |> update_unikernel
+  |> delete_unikernel
+  |> list_unikernels
+  |> start_unikernel
+  |> start_unikernel_level
+  |> stop_unikernel
+  |> App.run_command
